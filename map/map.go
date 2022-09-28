eb408340cfb29f075141b38408a4da2148443bf3
RenShiWei <ren_shi_wei@qq.com>
2022-09-28 20:27:57 +0800

    CapSyncMap

---
+++ map/map.go
@@ -0,0 +1,19 @@
+// description:
+// @author renshiwei
+// Date: 2022/9/28 19:51
+
+package util
+
+import (
+	"sync"
+)
+
+//CapSyncMap 获取 sync.Map 的容量
+func CapSyncMap(syncMap *sync.Map) int {
+	count := 0
+	syncMap.Range(func(key, value interface{}) bool {
+		count++
+		return true
+	})
+	return count
+}
